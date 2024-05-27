67ebaa773b4add54e74a4a2c396bb94c29e4ee3c
AngeloC89 <Angelociulla89@gmail.com>
2024-05-27 17:12:53 +0200

    init exercise and made migrate on my datadase

---
+++ config/db_links.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+        "characters" => "characters",
+        "comics" => "comics",
+        "movies" => "movies",
+        "tv" => "tv",
+        "games" => "games",
+        "collectibles" => "collectibles",
+        "videos" => "videos",
+        "fans" => "fans",
+        "news" => "news",
+        "shop" => "shop",
+];
