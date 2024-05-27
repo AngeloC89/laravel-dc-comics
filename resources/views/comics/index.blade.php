e2315fdf49cd6ac38955b4c8f351e0e2a896037e
AngeloC89 <Angelociulla89@gmail.com>
2024-05-27 20:21:53 +0200

    print the show.blade if click on title of card

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -9,9 +9,9 @@
                 @foreach ($comics as $comic)
                     <div class="col-12 col-md-3 col-lg-2">
                     <div class="card my-5">
-                            <img src="{{ $comic['thumb'] }}" class="card-img-top img-fluid " alt="{{ $comic['title'] }}">
+                            <img src="{{ $comic->thumb }}" class="card-img-top img-fluid " alt="{{ $comic->title }}">
                             <div class="card-body px-0 py-3">
-                                <h5 class="card-title text-truncate text-uppercase text-left ">{{ $comic['title'] }}</h5>
+                                <h5 class="card-title text-truncate text-uppercase text-left "><a class="text-decoration-none text-white" href="{{route('comics.show', $comic['id'])}}">{{ $comic->title }}</a></h5>
                               
                             </div>
                         </div>
