c18c9749bfb4be280995c80287b5d0d4fa307b56
AngeloC89 <Angelociulla89@gmail.com>
2024-05-31 21:12:36 +0200

    fixed create

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -10,19 +10,19 @@
         @csrf
         <div class="mb-3">
             <label for="title" class="form-label text-white " >Titolo</label>
-            <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{old('title')}}" > 
+            <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{old('title')}}" required> 
             @error('title')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </div>
         <div class="mb-3">
             <label for="description" class="form-label text-white">Descrizione</label>
-            <textarea type="text" class="form-control @error('title') is-invalid @enderror" id="description" name="description" rows="3" >{{old('description')}}</textarea>
+            <textarea type="text" class="form-control @error('description') is-invalid @enderror" id="description" name="description" rows="3" >{{old('description')}}</textarea>
             
         </div>
         <div class="mb-3">
             <label for="thumb" class="form-label text-white">Immagine</label>
-            <input type="text" class="form-control @error('title') is-invalid @enderror" id="thumb" name="thumb"  value="   {{old('thumb')}}">
+            <input type="text" class="form-control @error('thumb') is-invalid @enderror" id="thumb" name="thumb"  value="{{old('thumb')}}" required>
          
             @error('thumb')
                 <div class="alert alert-danger">{{ $message }}</div>
@@ -30,7 +30,7 @@
         </div>
         <div class="mb-3">
             <label for="price" class="form-label text-white">Prezzo</label>
-            <input type="text" class="form-control @error('price') is-invalid @enderror" id="price" name="price"  value="{{old('price')}}">
+            <input type="text" class="form-control @error('price') is-invalid @enderror" id="price" name="price"  value="{{old('price')}}" required>
            
             @error('price')
                 <div class="alert alert-danger">{{ $message }}</div>
@@ -38,7 +38,7 @@
         </div>
         <div class="mb-3">
             <label for="series" class="form-label text-white ">Serie</label>
-            <input type="text" class="form-control @error('series') is-invalid @enderror" id="series" name="series" value="{{old('series')}}">
+            <input type="text" class="form-control @error('series') is-invalid @enderror" id="series" name="series" value="{{old('series')}}" required>
        
             @error('series')
                 <div class="alert alert-danger">{{ $message }}</div>
@@ -46,7 +46,7 @@
         </div>
         <div class="mb-3">
             <label for="sale_date" class="form-label text-white">Data di vendita</label>
-            <input type="text" class="form-control @error('sale_date') is-invalid @enderror" id="sale_date" name="sale_date" value=" {{old('sale_date')}}">
+            <input type="text" class="form-control @error('sale_date') is-invalid @enderror" id="sale_date" name="sale_date" value=" {{old('sale_date')}}" required>
            
             @error('sale_date')
                 <div class="alert alert-danger">{{ $message }}</div>
